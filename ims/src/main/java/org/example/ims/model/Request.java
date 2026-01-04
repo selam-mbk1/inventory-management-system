dd2de5b372f719a7e4b30ed0600b28ee88acd531
TadesseRahel <raheltaddese24@gmail.com>
2026-01-04 14:25:26 +0300

    Created data models for Request

---
+++ ims/src/main/java/org/example/ims/model/Request.java
@@ -0,0 +1,59 @@
+package org.example.ims.model;
+
+public class Request {
+
+    private int requestId;
+    private int requesterId;
+    private int itemId;
+    private int quantity;
+    private String reason;
+    private String status;
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getRequesterId() {
+        return requesterId;
+    }
+
+    public void setRequesterId(int requesterId) {
+        this.requesterId = requesterId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
